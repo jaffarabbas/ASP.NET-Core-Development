37b1403329c42305903f0e8d44e15335bfe0b37e
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-16 00:23:58 +0500

    Inserted Dtos/itemDtos.cs at 16/06/2022 00:23:58

---
+++ FirstWebAPIProject/Dtos/itemDtos.cs
@@ -0,0 +1,9 @@
+namespace FirstWebAPIProject.Dtos{
+    public record ItemDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
