05f919171fe17fd07683fca1f244e48ef3139630
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-16 00:23:59 +0500

    Updated Controllers/ItemConstroller.cs at 16/06/2022 00:23:59

--- FirstWebAPIProject/Controllers/ItemConstroller.cs
+++ FirstWebAPIProject/Controllers/ItemConstroller.cs
@@ -1,4 +1,6 @@
+using FirstWebAPIProject.Dtos;
 using FirstWebAPIProject.Entities;
+using FirstWebAPIProject.Extension;
 using FirstWebAPIProject.Repositries;
 using FirstWebAPIProject.repostries;
 using Microsoft.AspNetCore.Mvc;
@@ -15,18 +17,44 @@ namespace FirstWebAPIProject.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Item> GetItems(){
-            var item = _itemRepository.GetItems();
+        public IEnumerable<ItemDto> GetItems(){
+            var item = _itemRepository.GetItems().Select(item => item.ExItemDto());
             return item;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItems(Guid id){
+        public ActionResult<ItemDto> GetItems(Guid id){
             var item = _itemRepository.GetItem(id);
             if(item is null){
                 return NotFound();
             }
-            return Ok(item);
+            return item.ExItemDto();
         }
+
+        [HttpPost]
+        public ActionResult<ItemDto> CreateItem(CreateItemsDto createItemsDto){
+            Item item = new(){
+                Id = Guid.NewGuid(),
+                Name = createItemsDto.Name,
+                Price = createItemsDto.Price,
+                CreatedDate = DateTimeOffset.UtcNow,
+            };
+            _itemRepository.CreateItem(item);
+            return CreatedAtAction(nameof(GetItems),new {id = item.Id}, item.ExItemDto());
+        }
+
+        [HttpPut("{id}")]
+         public ActionResult UpdateItem(Guid id, UpdateItemsDto updateItemDto) {
+            var existingItem = _itemRepository.GetItem(id);
+            if(existingItem is null){
+                return NotFound();
+            }
+            Item updatedItem = existingItem with {
+                Name = updateItemDto.Name,
+                Price = updateItemDto.Price
+            };
+            _itemRepository.UpdateItem(updatedItem);
+            return NoContent();
+         }
     }
 }
