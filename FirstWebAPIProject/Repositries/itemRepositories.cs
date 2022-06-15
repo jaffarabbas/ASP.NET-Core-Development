692d97749ed2fe631fe85d2c5e9855f111cc20a2
Jaffar Abbas <gamaportal8@gmail.com>
2022-06-16 00:23:59 +0500

    Updated Repositries/itemRepositories.cs at 16/06/2022 00:23:59

--- FirstWebAPIProject/Repositries/itemRepositories.cs
+++ FirstWebAPIProject/Repositries/itemRepositories.cs
@@ -4,5 +4,7 @@ namespace FirstWebAPIProject.Repositries{
     public interface IItemRepositry{
         Item GetItem(Guid id);
         IEnumerable<Item> GetItems();
+        void CreateItem(Item item);
+        void UpdateItem(Item item);
     }
 }
