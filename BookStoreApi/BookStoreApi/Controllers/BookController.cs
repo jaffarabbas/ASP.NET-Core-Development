1a36caed886a9e07ee8e2984c105f37c663f5b3c
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-09 00:03:06 +0500

    Commited File : BookController.cs at 8/9/2022 12:03:06 AM

---
+++ BookStoreApi/BookStoreApi/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+}
