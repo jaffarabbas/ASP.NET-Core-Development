e57d7dda96f034e8cb40610aae1efdc32ba08b58
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-10 13:21:39 +0500

    Commited File : BookStoreContext.cs at 8/10/2022 1:21:38 PM

---
+++ BookStoreApi/BookStoreApi/Data/BookStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Books> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=.;Database=BookStoreApi;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
