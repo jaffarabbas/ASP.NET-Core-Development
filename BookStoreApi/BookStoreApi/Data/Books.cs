38fde17332fd60c3c606b0920863dc63c6f50026
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-10 13:21:37 +0500

    Commited File : Books.cs at 8/10/2022 1:21:37 PM

---
+++ BookStoreApi/BookStoreApi/Data/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApi.Data
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
