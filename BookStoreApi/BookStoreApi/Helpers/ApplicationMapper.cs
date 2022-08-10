4a7fee6c3267a6aa75948aae560b7e381ed73c6d
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-10 13:21:40 +0500

    Commited File : ApplicationMapper.cs at 8/10/2022 1:21:40 PM

---
+++ BookStoreApi/BookStoreApi/Helpers/ApplicationMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookStoreApi.Data;
+using BookStoreApi.Models;
+
+namespace BookStoreApi.Helpers
+{
+    public class ApplicationMapper : Profile
+    {
+        public ApplicationMapper()
+        {
+            CreateMap<Books, BookModel>().ReverseMap();
+        }
+    }
+}
