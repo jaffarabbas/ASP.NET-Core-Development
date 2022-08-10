a51c459a0c7b1fbd66af49d4380c745263d51aee
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-10 13:23:19 +0500

    Commited File : app.module.ts at 8/10/2022 1:23:19 PM

---
+++ BookStoreApi/BookStoreWebApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AllBooksComponent } from './all-books/all-books.component';
+import { AddBooksComponent } from './add-books/add-books.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AllBooksComponent,
+    AddBooksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
