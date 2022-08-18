c78403d5550c9825156ab356899225ca2fa773c2
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 16:52:18 +0500

    Commited File : BookModel.cs at 8/18/2022 4:52:17 PM

---
+++ EmployeesApi/EmployeesApi/Models/BookModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApi.Models
+{
+    public class BookModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
