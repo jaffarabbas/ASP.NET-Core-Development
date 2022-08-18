9bc203f3584c840ecdd79e2532ebebca235c4abe
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 16:52:15 +0500

    Commited File : BookController.cs at 8/18/2022 4:52:14 PM

---
+++ EmployeesApi/EmployeesApi/Controllers/BookController.cs
@@ -0,0 +1,69 @@
+﻿using BookStoreApi.Models;
+using BookStoreApi.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetAllBookAsAsync()
+        {
+            var books = await _bookRepository.GetAllBookAsAsync();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBookFromIdAsAsync([FromRoute]int id)
+        {
+            var book = await _bookRepository.GetBookFromIdAsAsync(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddBookAsAsync([FromBody]BookModel bookModel)
+        {
+            var id = await _bookRepository.AddBooksAsync(bookModel);
+            return CreatedAtAction(nameof(GetBookFromIdAsAsync), new {id = id , controller = "book" },id);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBookAsAsync([FromRoute] int id,[FromBody] BookModel bookModel)
+        {
+            await _bookRepository.UpdateBookFromIdAsAsync(id,bookModel);
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> UpdateBookPatchAsAsync([FromRoute] int id, [FromBody] JsonPatchDocument bookModel)
+        {
+            await _bookRepository.UpdateBookFromPatchIdAsAsync(id, bookModel);
+            return Ok();
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBookByIdAsAsync([FromRoute] int id)
+        {
+            await _bookRepository.DeleteBooksAsync(id);
+            return Ok();
+        }
+    }
+}
+ 
