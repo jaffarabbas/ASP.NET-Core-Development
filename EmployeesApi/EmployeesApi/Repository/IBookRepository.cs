7cfa9ee6ebe8fdb65b5a40ee86ec7e64ccf0e0d2
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-18 16:52:20 +0500

    Commited File : IBookRepository.cs at 8/18/2022 4:52:20 PM

---
+++ EmployeesApi/EmployeesApi/Repository/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookStoreApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.Repository
+{
+    public interface IBookRepository
+    {
+        Task<List<BookModel>> GetAllBookAsAsync();
+        Task<BookModel> GetBookFromIdAsAsync(int id);
+        Task<int> AddBooksAsync(BookModel bookModel);
+        Task UpdateBookFromIdAsAsync(int id, BookModel bookModel);
+        Task UpdateBookFromPatchIdAsAsync(int id, JsonPatchDocument bookModel);
+        Task DeleteBooksAsync(int id);
+    }
+}
