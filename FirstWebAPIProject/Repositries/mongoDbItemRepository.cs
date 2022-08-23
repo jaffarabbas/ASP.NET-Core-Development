c33bc2173f5834b545b668670020b746a0d89a34
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-23 17:21:48 +0500

    Commited File : mongoDbItemRepository.cs at 8/23/2022 5:21:48 PM

---
+++ FirstWebAPIProject/Repositries/mongoDbItemRepository.cs
@@ -0,0 +1,23 @@
+namespace FirstWebAPIProject.Repositries
+{
+    public class MongoDbItemRepository : IItemRepositry
+    {
+        public IEnumerable<Item> GetItems()
+        {
+           
+        }
+
+        public Item GetItem(Guid id)
+        {
+           
+        }
+
+        public void CreateItem(Item item)
+        {
+        }
+
+        public void UpdateItem(Item item)
+        {
+        }
+    }
+}
