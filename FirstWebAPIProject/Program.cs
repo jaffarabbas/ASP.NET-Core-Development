f569051cab29e16f13060ccc90ba4551886e1105
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-25 15:30:59 +0500

    Commited File : Program.cs at 8/25/2022 3:30:58 PM

--- FirstWebAPIProject/Program.cs
+++ FirstWebAPIProject/Program.cs
@@ -1,5 +1,4 @@
 using FirstWebAPIProject.Repositries;
-using FirstWebAPIProject.repostries;
 using FirstWebAPIProject.Settings;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
@@ -11,7 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;   
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
