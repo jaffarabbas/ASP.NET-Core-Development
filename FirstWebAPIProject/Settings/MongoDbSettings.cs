87587f7e02a12bcf79971935a1cfea6d060cc9ae
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-25 23:46:48 +0500

    Commited File : MongoDbSettings.cs at 8/25/2022 11:46:48 PM

--- FirstWebAPIProject/Settings/MongoDbSettings.cs
+++ FirstWebAPIProject/Settings/MongoDbSettings.cs
@@ -4,12 +4,14 @@
     {
         public string? Host { get; set; }
         public string? Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
 
         public string ConnectionString
         {
             get
             {
-                return $"mongodb://{Host}:{Port}";
+                return $"mongodb://{User}:{Password}@{Host}:{Port}";
             }
         }
     }
