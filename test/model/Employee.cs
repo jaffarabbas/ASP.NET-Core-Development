255234837a385618a74d4034165bcc0155f51292
jaffarabbas <gamaportal8@gmail.com>
2022-11-02 20:42:55 +0500

    Inserted model/Employee.cs at 02/11/2022 20:42:55

---
+++ test/model/Employee.cs
@@ -0,0 +1,9 @@
+namespace test.model{
+    public class Employee{
+        public int id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string designation { get; set; }
+    }
+}
