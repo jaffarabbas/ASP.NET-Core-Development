4893dfa7f5f8b63485411346bedb23862474445e
jaffarabbas <gamaportal8@gmail.com>
2022-11-02 20:42:55 +0500

    Inserted repository/EmployeeRepository.cs at 02/11/2022 20:42:55

---
+++ test/repository/EmployeeRepository.cs
@@ -0,0 +1,15 @@
+using test.repository;
+using test.model;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test.repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        
+    }
+}
