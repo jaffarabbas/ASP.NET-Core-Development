11ec463771ceaa92f53b017f43a5e5842b386500
jaffarabbas <gamaportal8@gmail.com>
2022-11-06 13:56:34 +0500

    Updated repository/EmployeeRepository.cs at 06/11/2022 13:56:34

--- test/repository/EmployeeRepository.cs
+++ test/repository/EmployeeRepository.cs
@@ -10,6 +10,31 @@ namespace test.repository
 {
     public class EmployeeRepository : IEmployeeRepository
     {
-        
+        private List<Employee> employees = new List<Employee>();
+        public Task<List<Employee>> GetAllEmployeeAsAsync()
+        {
+            return employees;
+        }
+
+        public Task<Employee> GetEmployeeFromIdAsAsync(int id)
+        {
+            Employee emp = employees.Find(employees.id == id);
+            return emp;
+        }
+
+        public Task<int> AddEmployeesAsync(Employee employeeModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployeeFromIdAsAsync(int id, Employee employeeModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteBooksAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
