6601755937ec690467cbb10ed0eacfb13fe464ab
jaffarabbas <gamaportal8@gmail.com>
2022-11-06 13:56:34 +0500

    Updated repository/IEmployeeRepository.cs at 06/11/2022 13:56:34

--- test/repository/IEmployeeRepository.cs
+++ test/repository/IEmployeeRepository.cs
@@ -1,14 +1,13 @@
 using test.model;
-using Microsoft.AspNetCore.JsonPatch;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace test.repository{
     public interface IEmployeeRepository{
-        public Task<List<Employee>> GetAllEmployeeAsAsync();
-        public Task<EmployeeModel> GetEmployeeFromIdAsAsync(int id);
-        public Task<int> AddEmployeesAsync(Employee employeeModel);
-        public Task UpdateEmployeeFromIdAsAsync(int id, Employee employeeModel);        
-        public Task DeleteBooksAsync(int id);
+      Task<List<Employee>> GetAllEmployeeAsAsync();
+      Task<Employee> GetEmployeeFromIdAsAsync(int id);
+      Task<int> AddEmployeesAsync(Employee employeeModel);
+      Task UpdateEmployeeFromIdAsAsync(int id, Employee employeeModel);        
+      Task DeleteBooksAsync(int id);
     }
 }
