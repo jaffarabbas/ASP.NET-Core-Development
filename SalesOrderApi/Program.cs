c5bf5ad194a1b5b55838ba22edf2e2a7bf74691a
jaffarabbas <gamaportal8@gmail.com>
2022-11-16 22:35:28 +0500

    Updated Program.cs at 16/11/2022 22:35:28

--- SalesOrderApi/Program.cs
+++ SalesOrderApi/Program.cs
@@ -1,3 +1,6 @@
+using SalesOrderApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<Sales_DBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetConnectionString("connection"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,3 +28,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
