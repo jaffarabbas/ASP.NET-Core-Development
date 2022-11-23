c680036b8921f55fa946985d0ee9dbe83486fd28
jaffarabbas <gamaportal8@gmail.com>
2022-11-23 21:17:16 +0500

    Updated Repository/CustomerRepository/CustomersRepository.cs at 23/11/2022 21:17:16

--- SalesOrderApi/Repository/CustomerRepository/CustomersRepository.cs
+++ SalesOrderApi/Repository/CustomerRepository/CustomersRepository.cs
@@ -1,17 +1,24 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using SalesOrderApi.Models;
 
 namespace SalesOrderApi.Repository{
     public class CustomerRespository : ICustomerRepository{
         private readonly Sales_DBContext _dBContext;
-        public CustomerRespository(Sales_DBContext context)
+        private readonly IMapper mapper;
+        public CustomerRespository(Sales_DBContext context,IMapper mapper)
         {
             this._dBContext = context;
+            this.mapper = mapper;
         }
 
         public async Task<List<CustomerEntity>> GetAll()
         {
-            return await this._dBContext.TblCustomers.ToListAsync();
+            var customerData = await this._dBContext.TblCustomers.ToListAsync();
+            if(customerData != null && customerData.Count > 0){
+                return this.mapper.Map<List<TblCustomer>,List<CustomerEntity>>(customerData);
+            }
+            return new List<CustomerEntity>();
         }
     }
 }
