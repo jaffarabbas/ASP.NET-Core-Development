0c4efa15c92a1aa286cae8e5ae2edba390503b93
jaffarabbas <gamaportal8@gmail.com>
2022-11-26 17:13:19 +0500

    Update InvoiceRepository.cs

--- SalesOrderApi/Repository/InvoiceRepository/InvoiceRepository.cs
+++ SalesOrderApi/Repository/InvoiceRepository/InvoiceRepository.cs
@@ -2,32 +2,64 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using SalesOrderApi.Models;
 
-namespace SalesOrderApi.Repository{
+namespace SalesOrderApi.Repository
+{
     public class InvoiceRepository : IInvoiceRepository
     {
-        public Task<List<InvoiceDetails>> GetAllInvoiceDetailsByCode(string invoiceCode)
+        private readonly Sales_DBContext _dBContext;
+        private readonly IMapper mapper;
+        public InvoiceRepository(Sales_DBContext context, IMapper mapper)
         {
-            throw new NotImplementedException();
+            this._dBContext = context;
+            this.mapper = mapper;
+        }
+        public async Task<List<InvoiceHeader>> GetAllInvoiceHeader()
+        {
+            var customerInvoiceHeaderData = await this._dBContext.TblSalesHeaders.ToListAsync();
+            if (customerInvoiceHeaderData != null && customerInvoiceHeaderData.Count > 0)
+            {
+                return this.mapper.Map<List<TblSalesHeader>, List<InvoiceHeader>>(customerInvoiceHeaderData);
+            }
+            return new List<InvoiceHeader>();
         }
 
-        public Task<List<InvoiceHeader>> GetAllInvoiceHeader()
+        public async Task<InvoiceHeader> GetAllInvoiceHeaderByCode(string invoiceCode)
         {
-            throw new NotImplementedException();
+            var customerInvoiceHeaderData = await this._dBContext.TblSalesHeaders.FirstOrDefaultAsync(item => item.InvoiceNo == invoiceCode);
+            if (customerInvoiceHeaderData != null)
+            {
+                return this.mapper.Map<TblSalesHeader, InvoiceHeader>(customerInvoiceHeaderData);
+            }
+            return new InvoiceHeader();
+        }
+        public async Task<List<InvoiceDetails>> GetAllInvoiceDetailsByCode(string invoiceCode)
+        {
+            var customerInvoiceDetailsData = await this._dBContext.TblSalesProductInfos.Where(item => item.InvoiceNo == invoiceCode).ToListAsync();
+            if (customerInvoiceDetailsData != null && customerInvoiceDetailsData.Count > 0)
+            {
+                return this.mapper.Map<List<TblSalesProductInfo>, List<InvoiceDetails>>(customerInvoiceDetailsData);
+            }
+            return new List<InvoiceDetails>();
         }
 
-        public Task<InvoiceHeader> GetAllInvoiceHeaderByCode(string invoiceCode)
+
+        public async Task<ResponseType> Remove(string invoiceCode)
         {
-            throw new NotImplementedException();
+            return new ResponseType();
         }
 
-        public Task<ResponseType> Remove(string invoiceCode)
-        {
-            throw new NotImplementedException();
+        private async Task<string> SaveHeader(InvoiceHeader invoiceHeader){
+            string Result = string.Empty;
+            return Result;
         }
 
-        public Task<ResponseType> Save(InvoiceEntity invoiceEntity)
+        private async Task<string> SaveDetails(InvoiceDetails invoiceDetails){
+            string Result = string.Empty;
+            return Result;
+        }
+        public async Task<ResponseType> Save(InvoiceEntity invoiceEntity)
         {
-            throw new NotImplementedException();
+            return new ResponseType();
         }
     }
 }
