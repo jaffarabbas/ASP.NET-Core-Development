75d9970fd2499ca8c04a1270b248c2426666031f
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-18 21:50:28 +0500

    Commited File : GitController.cs at 12/18/2022 9:50:28 PM

---
+++ GitCommiterApi/GitCommiterApi/Controllers/GitController.cs
@@ -0,0 +1,25 @@
+﻿using GitCommiterApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitCommiterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitController : ControllerBase
+    {
+        private readonly IGitRepository _gitRepository;
+
+        public GitController(IGitRepository git)
+        {
+            this._gitRepository = git;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetData()
+        {
+            var data = await _gitRepository.gitRepositoryList();
+            return Ok(data);
+        }
+    }
+}
