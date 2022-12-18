b69e8b3a28bca671739e9a7c10ebf5b21fc09e9f
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-18 21:50:29 +0500

    Commited File : GitRepository.cs at 12/18/2022 9:50:29 PM

---
+++ GitCommiterApi/GitCommiterApi/Repositories/GitRepository.cs
@@ -0,0 +1,34 @@
+﻿using GitCommiterMethods;
+
+namespace GitCommiterApi.Repositories
+{
+    public class GitRepository : IGitRepository
+    {
+        private readonly Commiter _gitCommiter;
+        public GitRepository(Commiter commiter) { 
+            this._gitCommiter = commiter;
+        }
+
+        public async Task<Dictionary<int, string>> gitRepositoryList() 
+        {
+            try
+            {
+                return _gitCommiter.GitPathList();
+            }
+            catch(Exception e)
+            {
+                return new Dictionary<int, string>()
+                {
+                    {
+                         0 , ""+e.ToString()
+                    }
+                };
+            }
+        }
+
+        public Task<List<string>> staggingfileList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
