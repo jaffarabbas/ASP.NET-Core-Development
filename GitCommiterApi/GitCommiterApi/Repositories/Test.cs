732a27f487dc6845e90ad698ff7a63a5f283575e
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-20 17:45:31 +0500

    Commited File : Test.cs at 12/20/2022 5:45:31 PM

---
+++ GitCommiterApi/GitCommiterApi/Repositories/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace GitCommiterApi.Repositories
+{
+    public class Test : ITest
+    {
+        List<string> lst = new List<string>()
+        {
+            "1","2"
+        };
+        public Task<List<string>> data()
+        {
+            return Task.FromResult(lst);
+        }
+    }
+}
