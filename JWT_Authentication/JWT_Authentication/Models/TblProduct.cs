4efc5bb4d229dc405b6a8c0c23e4acdc7bf06d29
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:20 +0500

    Commited File : TblProduct.cs at 12/29/2022 11:50:19 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblProduct
+{
+    public int Code { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Amount { get; set; }
+}
