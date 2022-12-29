bc1cfe9947acca6fec35c3badecb159d32037b1a
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:16 +0500

    Commited File : UserController.cs at 12/29/2022 11:50:16 PM

---
+++ JWT_Authentication/JWT_Authentication/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using JWT_Authentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Authentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly JwtAuthentictionContext _context;
+        private readonly JWTSetting _setting;
+        public UserController(JwtAuthentictionContext context)
+        {
+            this._context = context;
+        }
+        [Route("Authentication")]
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] User user)
+        {
+
+            return Ok();
+        }
+    }
+}
