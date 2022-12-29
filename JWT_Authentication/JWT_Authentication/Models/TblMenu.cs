05ecbae5a84ddf2f77c0b37d5a852804f82d07cf
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:19 +0500

    Commited File : TblMenu.cs at 12/29/2022 11:50:18 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblMenu
+{
+    public string Id { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? LinkName { get; set; }
+}
