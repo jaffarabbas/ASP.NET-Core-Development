c86e7363d09475bf7da0340ef5cfe36ecaf00e5f
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:24 +0500

    Commited File : Program.cs at 12/29/2022 11:50:24 PM

---
+++ JWT_Authentication/JWT_Authentication/Program.cs
@@ -0,0 +1,34 @@
+using JWT_Authentication.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<JwtAuthentictionContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("connection")));
+
+var _jwtSettng = builder.Configuration.GetConnectionString("JWTSetting");
+
+builder.Services.Configure<JWTSetting>(_jwtSettng);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
