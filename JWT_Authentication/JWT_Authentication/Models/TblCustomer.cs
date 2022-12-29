0bd1ed4f28b73aac2ed57ef98635f5b04cc2050e
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:17 +0500

    Commited File : TblCustomer.cs at 12/29/2022 11:50:17 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblCustomer
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? CreditLimit { get; set; }
+}
