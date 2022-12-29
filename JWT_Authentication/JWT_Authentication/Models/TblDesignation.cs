c98d81baf54eb464c96814f108fec6ef3abba2c0
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:18 +0500

    Commited File : TblDesignation.cs at 12/29/2022 11:50:17 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblDesignation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblDesignation
+{
+    public string Code { get; set; } = null!;
+
+    public string? Name { get; set; }
+}
