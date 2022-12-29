bc49368a8519a626f3bdd1094796ca6fa13a385d
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:15 +0500

    Commited File : CustomerController.cs at 12/29/2022 11:50:15 PM

---
+++ JWT_Authentication/JWT_Authentication/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using JWT_Authentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Authentication.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly JwtAuthentictionContext _context;
+        public CustomerController(JwtAuthentictionContext context) {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<TblCustomer> GetAll()
+        {
+            return this._context.TblCustomers.ToList();
+        }
+    }
+}
