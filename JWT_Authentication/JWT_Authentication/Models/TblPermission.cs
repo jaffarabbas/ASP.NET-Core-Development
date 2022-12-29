7988133d7ff4b973854a31070a9eb7829a8e4a85
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:19 +0500

    Commited File : TblPermission.cs at 12/29/2022 11:50:19 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblPermission.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblPermission
+{
+    public string RoleId { get; set; } = null!;
+
+    public string MenuId { get; set; } = null!;
+}
