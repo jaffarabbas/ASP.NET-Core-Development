6c1abd59a45af68e4ab0d90de1bba8b752501409
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:18 +0500

    Commited File : TblEmployee.cs at 12/29/2022 11:50:18 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblEmployee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblEmployee
+{
+    public int Code { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Designation { get; set; }
+}
