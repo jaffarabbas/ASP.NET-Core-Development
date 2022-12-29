a66269a76da37dfbe9620dfbcf4147bf47fadc56
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-29 23:50:22 +0500

    Commited File : TblRole.cs at 12/29/2022 11:50:21 PM

---
+++ JWT_Authentication/JWT_Authentication/Models/TblRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JWT_Authentication.Models;
+
+public partial class TblRole
+{
+    public string Roleid { get; set; } = null!;
+
+    public string? Name { get; set; }
+}
