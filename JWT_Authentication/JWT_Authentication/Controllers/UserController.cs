c7348deddf409c9dfc6cce92a24178536685a125
Jaffar Abbas <gamaportal8@gmail.com>
2022-12-30 13:04:13 +0500

    Commited File : UserController.cs at 12/30/2022 1:04:12 PM

--- JWT_Authentication/JWT_Authentication/Controllers/UserController.cs
+++ JWT_Authentication/JWT_Authentication/Controllers/UserController.cs
@@ -1,6 +1,11 @@
 ﻿using JWT_Authentication.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JWT_Authentication.Controllers
 {
@@ -10,16 +15,37 @@ namespace JWT_Authentication.Controllers
     {
         private readonly JwtAuthentictionContext _context;
         private readonly JWTSetting _setting;
-        public UserController(JwtAuthentictionContext context)
+        public UserController(JwtAuthentictionContext context,IOptions<JWTSetting> settings)
         {
             this._context = context;
+            this._setting = settings.Value;
         }
-        [Route("Authentication")]
+        [Route("Authenticate")]
         [HttpPost]
         public IActionResult Authenticate([FromBody] User user)
         {
+            var _user = this._context.TblUsers.FirstOrDefault(data => data.Userid == user.username && data.Password == user.password);
+            if (_user == null)
+                return Unauthorized();
 
-            return Ok();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenkey = Encoding.UTF8.GetBytes(this._setting.securitykey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                            new Claim(ClaimTypes.Name, _user.Userid),
+                    }
+                ),
+                Expires = DateTime.Now.AddMinutes(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenkey
+                ), SecurityAlgorithms.HmacSha256)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            string finaltoken = tokenHandler.WriteToken(token);
+            return Ok(finaltoken);
         }
     }
 }
