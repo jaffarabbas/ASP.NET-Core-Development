a0a7ebdb61ccf839ddc645641973d1f11643a718
jaffarabbas <gamaportal8@gmail.com>
2023-08-05 20:46:34 +0500

    Commited File : Sale.cs at 05/08/2023 8:46:33 pm

---
+++ TodoApp/Models/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApp.Models
+{
+    public partial class Sale
+    {
+        public int Sid { get; set; }
+        public DateTime Sdate { get; set; }
+        public int Cid { get; set; }
+        public int Iid { get; set; }
+
+        public virtual Customer CidNavigation { get; set; } = null!;
+        public virtual Item IidNavigation { get; set; } = null!;
+    }
+}
