21e3aabade595df2ae8c632e105409285ab76319
jaffarabbas <gamaportal8@gmail.com>
2023-08-05 20:46:35 +0500

    Commited File : Program.cs at 05/08/2023 8:46:34 pm

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,8 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+var provider = builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<dbMvcContext>(item => item.UseSqlServer(config.GetConnectionString("dbString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
