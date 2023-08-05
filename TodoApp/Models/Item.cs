2a289f6916f39727e66e883a80a72de4a1823044
jaffarabbas <gamaportal8@gmail.com>
2023-08-05 20:46:33 +0500

    Commited File : Item.cs at 05/08/2023 8:46:33 pm

---
+++ TodoApp/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApp.Models
+{
+    public partial class Item
+    {
+        public Item()
+        {
+            Sales = new HashSet<Sale>();
+        }
+
+        public int ItemId { get; set; }
+        public string Iname { get; set; } = null!;
+        public string Idescription { get; set; } = null!;
+        public decimal? Price { get; set; }
+        public int? Quantity { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
