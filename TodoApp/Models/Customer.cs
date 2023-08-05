73d57f3a109496fbad71a24e870b6dde9f080232
jaffarabbas <gamaportal8@gmail.com>
2023-08-05 20:46:33 +0500

    Commited File : Customer.cs at 05/08/2023 8:46:32 pm

---
+++ TodoApp/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApp.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Sales = new HashSet<Sale>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
