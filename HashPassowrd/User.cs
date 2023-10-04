f994c25d9a1c8d2a42c73c021aad82c225d0c87c
jaffarabbas <gamaportal8@gmail.com>
2023-10-04 21:06:57 +0500

    Commited File : User.cs at 04/10/2023 9:06:56 pm

---
+++ HashPassowrd/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPassowrd
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] Salt { get; set; }
+    }
+}
