35af622c92cbbf3671b5b35ea016c11e083241e6
jaffarabbas <gamaportal8@gmail.com>
2023-10-24 22:54:03 +0500

    Commited File : Program.cs at 24/10/2023 10:54:03 pm

---
+++ SchemaExtracter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string apiUrl = "https://localhost:7120/api/";
+        var httpClient = new HttpClient();
+
+        // Send a GET request to the API URL
+        var response = await httpClient.GetAsync(apiUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseBody = await response.Content.ReadAsStringAsync();
+            await Console.Out.WriteLineAsync(responseBody);
+            // Parse responseBody to extract schema and endpoint information
+            // Store the extracted data in data structures for further use
+            // Implement logic to discover and analyze endpoints
+        }
+        else
+        {
+            Console.WriteLine($"HTTP Error: {response.StatusCode}");
+        }
+    }
+}
