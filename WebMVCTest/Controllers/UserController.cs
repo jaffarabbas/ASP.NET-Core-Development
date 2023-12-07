b88810a12b9064b9922e015d662d42bd1308735d
jaffarabbas <gamaportal8@gmail.com>
2023-12-07 22:49:03 +0500

    07-12-2023

---
+++ WebMVCTest/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCTest.Models;
+
+namespace WebMVCTest.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var data = new User()
+            {
+                id = id,
+                user = "asdasd",
+                email = "asda2@asda.com"
+            };
+            return View(data);
+        }
+    }
+}
---
+++ WebMVCTest/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace WebMVCTest.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string user { get; set; }
+        public string email { get; set; }
+    }
+}
