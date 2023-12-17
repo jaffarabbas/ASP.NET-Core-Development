cb1bb61d24fd77a1aafd17c7c34d1b399ab32f57
jaffarabbas <gamaportal8@gmail.com>
2023-12-17 13:47:53 +0500

    Update working add api health check

---
+++ GenericRepositoryPatternApi/HealthCheck/ApiHealthCheck.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GenericRepositoryPatternApi.HealthCheck
+{
+    public class ApiHealthCheck : IHealthCheck
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiHealthCheck(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var res = await _httpClient.GetAsync("https://localhost:7209/api/ProductWithUOW");
+            if (res.IsSuccessStatusCode)
+            {
+                return await Task.FromResult(new HealthCheckResult(
+                    status: HealthStatus.Healthy,
+                    description: "Api is up and running"
+                    ));
+            }
+            return await Task.FromResult(new HealthCheckResult(
+                    status: HealthStatus.Unhealthy,
+                    description: "Api is down"
+                    ));
+        }
+    }
+}
--- GenericRepositoryPatternApi/Program.cs
+++ GenericRepositoryPatternApi/Program.cs
@@ -1,12 +1,18 @@
+using GenericRepositoryPatternApi.HealthCheck;
 using GenericRepositoryPatternApi.Models;
 using GenericRepositoryPatternApi.Repository;
 using GenericRepositoryPatternApi.Repository.UOW;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddHttpClient();
 
 #region database initialization
 
@@ -20,12 +26,25 @@ builder.Services.AddDbContext<DbJewelsiteContext>(options =>
 
 #endregion 
 
+#region API Health check
+
+builder.Services.AddHealthChecks()
+    .AddCheck<ApiHealthCheck>(nameof(ApiHealthCheck))
+    .AddDbContextCheck<DbJewelsiteContext>();
+
+#endregion
+
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecksUI(options =>
+{
+    options.AddHealthCheckEndpoint("HealthCheck API", "/healthchecks");
+}).AddInMemoryStorage();
 
 var app = builder.Build();
 
@@ -37,10 +56,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapHealthChecks("/health", new HealthCheckOptions()
+{
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+    }
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+#region Configure health check ui
+
+app.MapHealthChecks("/healthchecks", new()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.MapHealthChecksUI(options => options.UIPath = "/healthCheckDashboard");
+
+#endregion
 
 app.Run();
