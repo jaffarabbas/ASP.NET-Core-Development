f3dc5a22429887e64c0a168b06da43872fc32a59
jaffarabbas <gamaportal8@gmail.com>
2024-02-10 22:38:19 +0500

    Commited File : RolesController.cs at 10/02/2024 10:38:19 pm

---
+++ IdentityManagerApi/IdentityManagerApi/Controllers/RolesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        [HttpGet("Admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAdmin()
+        {
+            return Ok("Is Admin");
+        }
+
+        [HttpGet("User")]
+        [Authorize(Roles = "User")]
+        public IActionResult GetUser()
+        {
+            return Ok("Is User");
+        }
+    }
+}
