a79df932879ef3a7ecf359d441c1748dca0ba6d7
jaffarabbas <gamaportal8@gmail.com>
2024-02-10 22:38:18 +0500

    Commited File : AccountController.cs at 10/02/2024 10:38:18 pm

---
+++ IdentityManagerApi/IdentityManagerApi/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using CustomShareProject.Contracts;
+using CustomShareProject.DTOS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(IUserAccount userAccount) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserDtos userDTO)
+        {
+            var response = await userAccount.CreateAccount(userDTO);
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            var response = await userAccount.LoginAccount(loginDTO);
+            return Ok(response);
+        }
+    }
+}
