3291f6654ca581085d9424a5449923906c2f5ce0
jaffarabbas <gamaportal8@gmail.com>
2024-02-10 22:38:16 +0500

    Commited File : UserSession.cs at 10/02/2024 10:38:16 pm

---
+++ IdentityManagerApi/CustomShareProject/DTOS/UserSession.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomShareProject.DTOS
+{
+    public record class UserSession(string? Id,string? Name,string? Email,string? Role);
+}
