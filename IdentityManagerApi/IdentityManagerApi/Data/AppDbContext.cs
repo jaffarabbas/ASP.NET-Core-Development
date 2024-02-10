cf200fe076ba23064e04fa2d8bade0fb810bf0aa
jaffarabbas <gamaportal8@gmail.com>
2024-02-10 22:38:20 +0500

    Commited File : AppDbContext.cs at 10/02/2024 10:38:19 pm

---
+++ IdentityManagerApi/IdentityManagerApi/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityManagerApi.Data
+{
+    public class AppDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
