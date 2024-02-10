1051b99582796c0d6be8b551d305a16d153ace31
jaffarabbas <gamaportal8@gmail.com>
2024-02-10 22:38:15 +0500

    Commited File : ServiceResponces.cs at 10/02/2024 10:38:15 pm

---
+++ IdentityManagerApi/CustomShareProject/DTOS/ServiceResponces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomShareProject.DTOS
+{
+    public class ServiceResponces
+    {
+        public record class GeneralResponse(bool Flag,string Message);
+        public record class LoginResponse(bool Flag,string Token,string Message);
+    }
+}
