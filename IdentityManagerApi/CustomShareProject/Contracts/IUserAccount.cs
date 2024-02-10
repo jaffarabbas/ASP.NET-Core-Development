5caefccd98cd70c1e249ff4e537eeea8b665d7d1
jaffarabbas <gamaportal8@gmail.com>
2024-02-10 22:38:13 +0500

    Commited File : IUserAccount.cs at 10/02/2024 10:38:13 pm

---
+++ IdentityManagerApi/CustomShareProject/Contracts/IUserAccount.cs
@@ -0,0 +1,16 @@
+﻿using CustomShareProject.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CustomShareProject.DTOS.ServiceResponces;
+
+namespace CustomShareProject.Contracts
+{
+    public interface IUserAccount
+    {
+        Task<GeneralResponse> CreateAccount(UserDtos userDtos);
+        Task<LoginResponse> LoginAccount(LoginDTO loginDTO);
+    }
+}
